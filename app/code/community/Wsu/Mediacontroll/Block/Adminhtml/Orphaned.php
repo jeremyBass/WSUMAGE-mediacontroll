939e7b817d183aed8a5cedf9730603a67587b4ae
Jeremy Bass <jeremybass@cableone.net>
2014-04-12 14:03:17 -0700

    sets up the Orphaned tab block

---
+++ app/code/community/Wsu/Mediacontroll/Block/Adminhtml/Orphaned.php
@@ -0,0 +1,20 @@
+<?php
+
+class Wsu_Mediacontroll_Block_Adminhtml_Orphaned extends Mage_Adminhtml_Block_Widget_Grid_Container{
+    public function __construct(){
+        $this->_controller = 'adminhtml_store';
+        $this->_blockGroup = 'wsu_mediacontroll';
+        $this->_headerText = Mage::helper('mediacontroll')->__('Find Orphaned Images');
+        parent::__construct();
+        $this->_removeButton('add');
+        $this->setTemplate('wsu/mediacontroll/widget/grid/container.phtml');
+    }
+
+    public function getCreateUrl() {
+        return '';
+    }
+
+    public function getHeaderCssClass() {
+        return '';
+    }
+}
