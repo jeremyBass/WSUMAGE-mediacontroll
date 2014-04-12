3bfc3fc72b9f1f0c55c0fbdd8538e4b829ab1846
Jeremy Bass <jeremybass@cableone.net>
2014-04-12 14:05:19 -0700

    start the index action

--- app/code/community/Wsu/Mediacontroll/controllers/Adminhtml/Mediacontoll.php
+++ app/code/community/Wsu/Mediacontroll/controllers/Adminhtml/Mediacontoll.php
@@ -1,5 +1,20 @@
 <?php
 class Wsu_Mediacontroll_Adminhtml_MediacontrollController extends Mage_Adminhtml_Controller_Action {
+
+    public function indexAction(){
+        $this->_title($this->__('Mediacontroll'));
+        $this->loadLayout();
+        $this->_setActiveMenu('system/mediacontroll');
+        $this->renderLayout();
+    }
+
+
+
+
+
+
+
+
 	
 	
 }
