18163358684879ff20ea237c14c1c453a9f17774
Jeremy Bass <jeremybass@cableone.net>
2014-04-12 13:46:27 -0700

    sets up construct

--- app/code/community/Wsu/Mediacontroll/Block/Adminhtml/Tabs.php
+++ app/code/community/Wsu/Mediacontroll/Block/Adminhtml/Tabs.php
@@ -1,8 +1,16 @@
 <?php
 
 class Wsu_Mediacontroll_Block_Adminhtml_Tabs extends Mage_Adminhtml_Block_Widget_Tabs {
-
-
+    public function __construct(){
+        parent::__construct();
+        $this->setId('mc_img_tabs');
+        $this->setTitle(Mage::helper('mediacontroll')->__('Image controls'));
+    }
+	
+	
+	
+	
+	
 }
 
 
