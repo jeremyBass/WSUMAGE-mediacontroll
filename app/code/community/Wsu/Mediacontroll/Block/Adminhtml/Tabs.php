84cbc150a0a9b83158a2b491fdd70d80e08200d5
Jeremy Bass <jeremybass@cableone.net>
2014-04-12 13:53:17 -0700

    sets up tabs

--- app/code/community/Wsu/Mediacontroll/Block/Adminhtml/Tabs.php
+++ app/code/community/Wsu/Mediacontroll/Block/Adminhtml/Tabs.php
@@ -7,7 +7,50 @@ class Wsu_Mediacontroll_Block_Adminhtml_Tabs extends Mage_Adminhtml_Block_Widget
         $this->setTitle(Mage::helper('mediacontroll')->__('Image controls'));
     }
 	
-	
+    protected function _beforeToHtml(){
+        $activeSection = $this->_getActiveSection();
+		/*
+		echo 'total products with images',' ',count($productImgCollection), PHP_EOL;
+		
+		echo 'total products missing starting sort idexes',' ',count(array_filter($productImgCollection, function($val){
+						return $val['productImageProfile']['hasSortIndexStart'];
+					})
+				), PHP_EOL;
+		echo 'total products missing sorted',' ',count(array_filter($productImgCollection, function($val){
+						return $val['productImageProfile']['missingSorted'];
+					})
+				), PHP_EOL;
+		echo 'total products missing assigned',' ',count(array_filter($productImgCollection, function($val){
+						return $val['productImageProfile']['missingAssigned'];
+					})
+				), PHP_EOL;
+		*/
+        $this->addTab('missingSorted_section', array(
+            'label'     => Mage::helper('mediacontroll')->__('Missing Sorted'),
+            'title'     => Mage::helper('mediacontroll')->__('Missing Sorted'),
+            'url'       => $this->getUrl('*/*/missingSorted', array('_current' => true)),
+            'class'     => 'ajax',
+            'active'    => $activeSection === 'missingSorted',
+        ));
+
+        $this->addTab('missingAssigned_section', array(
+            'label'     => Mage::helper('mediacontroll')->__('Missing Assigned'),
+            'title'     => Mage::helper('mediacontroll')->__('Missing Assigned'),
+            'url'       => $this->getUrl('*/*/missingAssigned', array('_current' => true)),
+            'class'     => 'ajax',
+            'active'    => $activeSection === 'missingAssigned',
+        ));
+
+        $this->addTab('missSorted_section', array(
+            'label'     => Mage::helper('mediacontroll')->__('Miss Sorted'),
+            'title'     => Mage::helper('mediacontroll')->__('Miss Sorted'),
+            'url'       => $this->getUrl('*/*/missSorted', array('_current' => true)),
+            'class'     => 'ajax',
+            'active'    => $activeSection === 'missSorted',
+        ));
+
+        return parent::_beforeToHtml();
+    }
 	
 	
 	
