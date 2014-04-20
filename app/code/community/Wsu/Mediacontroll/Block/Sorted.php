96c24d1472927787ac6f2b074dbaf22688c8b84a
Jeremy Bass <jeremybass@cableone.net>
2014-04-20 10:32:40 -0700

    a little clean up on the sort

--- app/code/community/Wsu/Mediacontroll/Block/Sorted.php
+++ app/code/community/Wsu/Mediacontroll/Block/Sorted.php
@@ -7,16 +7,6 @@ class Wsu_Mediacontroll_Block_Sorted extends Mage_Adminhtml_Block_Widget_Grid_Co
 		$this->_addButtonLabel = Mage::helper('mediacontroll')->__('Refresh');
 		parent::__construct();
 	}
-/*	
-	public function _prepareLayout() {
-		return parent::_prepareLayout();
-    }
-    */
-     public function getImgclean() { 
-        if (!$this->hasData('imgclean')) {
-            $this->setData('imgclean', Mage::registry('imgclean'));
-        }
-        return $this->getData('imgclean');
-    }
+
 }
 
