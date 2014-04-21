50c87de0f0828c5cb38dc6adf98a152e40ef98ff
jeremyBass <jeremybass@cableone.net>
2014-04-21 09:00:24 -0700

    sets up a controller for products missing images

---
+++ app/code/community/Wsu/MediaControll/controllers/MissingController.php
@@ -0,0 +1,124 @@
+<?php
+
+class Wsu_Mediacontroll_MissingController extends Mage_Adminhtml_Controller_action {
+
+
+	protected function _initAction() {
+		$this->loadLayout()->_setActiveMenu('system/tools')
+			->_addBreadcrumb(Mage::helper('adminhtml')->__('System'), Mage::helper('adminhtml')->__('System'))
+			->_addBreadcrumb(Mage::helper('adminhtml')->__('Tools'), Mage::helper('adminhtml')->__('Tools'))
+			->_addBreadcrumb(Mage::helper('mediacontroll')->__('Missing Images'), Mage::helper('mediacontroll')->__('Sort assignment'));
+		
+		return $this;
+	}   
+
+
+    public function indexAction() {
+		
+		
+        $this->_initAction()->_addContent($this->getLayout()->createBlock('mediacontroll/missing'));
+		
+		$this->renderLayout();
+    }
+/*	
+	public function idUnsortedAction() {
+		Mage::helper('mediacontroll')->get_ProductUnsortedImages();
+		
+		$this->_initAction()->_addContent($this->getLayout()->createBlock('mediacontroll/imgclean'));
+		
+		$this->renderLayout();
+	}
+
+
+
+
+
+
+public function indexAction() {
+		$this->_initAction()
+			->renderLayout();
+	}
+	*/
+	public function newAction(){
+		Mage::helper('mediacontroll')->compareList();
+		$this->_redirect('*/*/');
+	}
+
+
+
+//look to http://stackoverflow.com/a/4353686/746758
+//http://stackoverflow.com/a/19159776/746758
+	public function resortAction($id=0) {
+		$requestId = $this->getRequest()->getParam('id');
+		$prod_id = ($id > 0) ? $id : $requestId;
+		$affected = array();
+		$starting = 1;
+		if( $prod_id > 0 ) {
+			
+
+				$product = mage::getModel('catalog/product')->load($prod_id);
+				$attributes = $product->getTypeInstance(true)->getSetAttributes($product);
+				$gallery = $attributes['media_gallery'];
+				$images = $product->getMediaGalleryImages();
+				$i=$starting;
+				foreach ($images as $image) {
+					if($image->getDisabled()!='1'){
+						$backend = $gallery->getBackend();
+						$backend->updateImage(
+							$product,
+							$image->getFile(),
+							array('position' => $i)
+						);
+						Mage::getSingleton('adminhtml/session')->addSuccess(
+							Mage::helper('mediacontroll')->__('Setting sort to '.$i.' for '.$image->getFile())
+						);
+						$i++;
+					}
+				}
+			try {
+				if($i>1){
+					$product->getResource()->saveAttribute($product, 'media_gallery');
+					$product->save();
+					if($requestId>0){
+						Mage::getSingleton('adminhtml/session')->addSuccess(
+							Mage::helper('mediacontroll')->__('Product Images were successfully reSorted from 0-'.$i)
+							);
+					}
+				}else{
+					if($requestId>0){
+						Mage::getSingleton('adminhtml/session')->addSuccess(Mage::helper('mediacontroll')->__('Product Image wasn\'t able to be done.'));
+					}
+				}
+				$this->_redirect('*/*/');
+			} catch (Exception $e) {
+				Mage::getSingleton('adminhtml/session')->addError($e->getMessage());
+				$this->_redirect('*/*/');
+			}
+		}else{
+			Mage::getSingleton('adminhtml/session')->addError("failed to get key");
+			$this->_redirect('*/*/');
+		}
+	}
+    public function massResortAction() {
+        $mediacontrollIds = $this->getRequest()->getParam('mediacontroll');
+        if(!is_array($mediacontrollIds)) {
+			Mage::getSingleton('adminhtml/session')->addError(Mage::helper('adminhtml')->__('Please select item(s)'));
+        } else {
+            try {
+                foreach ($mediacontrollIds as $mediacontrollId) {
+					$this->resortAction($mediacontrollId);
+                }
+                Mage::getSingleton('adminhtml/session')->addSuccess(
+                    Mage::helper('adminhtml')->__(
+                        'Total of %d record(s) were successfully resorted', count($mediacontrollIds)
+                    )
+                );
+            } catch (Exception $e) {
+                Mage::getSingleton('adminhtml/session')->addError($e->getMessage());
+            }
+        }
+        $this->_redirect('*/*/index');
+    }
+
+	
+}
