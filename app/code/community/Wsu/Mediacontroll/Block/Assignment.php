209e5de9aec18b02ffea01b7b725d04783e64882
jeremyBass <jeremybass@cableone.net>
2014-04-21 08:20:49 -0700

    changes the name in the Assignment block

--- app/code/community/Wsu/Mediacontroll/Block/Assignment.php
+++ app/code/community/Wsu/Mediacontroll/Block/Assignment.php
@@ -3,7 +3,7 @@ class Wsu_Mediacontroll_Block_Assignment extends Mage_Adminhtml_Block_Widget_Gri
 	public function __construct() {
 		$this->_blockGroup = 'mediacontroll';
 		$this->_controller = 'assignment';
-		$this->_headerText = Mage::helper('mediacontroll')->__('Image Assessment');
+		$this->_headerText = Mage::helper('mediacontroll')->__('Image Assignment Assessment');
 		$this->_addButtonLabel = Mage::helper('mediacontroll')->__('Refresh');
 		parent::__construct();
 	}
