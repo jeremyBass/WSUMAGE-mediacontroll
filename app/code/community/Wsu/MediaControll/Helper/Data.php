d1cbac29279b84e20b50769b8aec7d03f9d0d6a1
jeremyBass <jeremybass@cableone.net>
2014-05-05 13:11:36 -0700

    had a few dies to remove

--- app/code/community/Wsu/MediaControll/Helper/Data.php
+++ app/code/community/Wsu/MediaControll/Helper/Data.php
@@ -160,7 +160,7 @@ class Wsu_Mediacontroll_Helper_Data extends Mage_Core_Helper_Abstract {
 		}
 		$prodcollection = Mage::getResourceModel('catalog/product_collection')
 			->addAttributeToFilter('status', array('eq' => Mage_Catalog_Model_Product_Status::STATUS_ENABLED));
-		$prodcollection->addAttributeToFilter('entity_id', array('nin' => $tracked_products));
+		if(!empty($tracked_products))$prodcollection->addAttributeToFilter('entity_id', array('nin' => $tracked_products));
 		$prodcollection->getSelect()->order('updated_at','ASC');
 
 		print( $prodcollection->getSelect() );//die();
@@ -289,11 +289,8 @@ class Wsu_Mediacontroll_Helper_Data extends Mage_Core_Helper_Abstract {
 					$newModel->setData(array('prod_id'=>$prodID,'imgprofile'=>json_encode($productArray)))->setId(null);
 					$newModel->save();
 				}
-				if($prodID==169)die();
-				
 			}
 		}
-		die('stopped at '.$type);
 		if($type=='orphaned'){
 			$model = Mage::getModel('wsu_mediacontroll/orphaned');	
 			$val	= $model->getCollection()->getImages();
