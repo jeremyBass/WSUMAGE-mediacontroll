b490ac8f075a855b52597699372916a84b6ca283
jeremyBass <jeremybass@cableone.net>
2014-05-05 07:37:53 -0700

    add an imgless and orphaned sql table

--- app/code/community/Wsu/MediaControll/sql/wsu_mediacontroll_setup/install-0.0.1.php
+++ app/code/community/Wsu/MediaControll/sql/wsu_mediacontroll_setup/install-0.0.1.php
@@ -38,4 +38,29 @@ $installer->run("
 		) ENGINE=InnoDB DEFAULT CHARSET=utf8; 
     ");
 
+$table_imgless = $installer->getTable('wsu_mediacontroll/imgless');
+$installer->run("
+    DROP TABLE IF EXISTS `{$table_imgless}`;
+    CREATE TABLE `{$table_imgless}` (
+			`imgless_id` int(11) unsigned NOT NULL auto_increment,
+			`prod_id` int(11) NOT NULL,
+			`imgprofile` text NOT NULL default '',
+			PRIMARY KEY  (`imgless_id`),
+			UNIQUE KEY `prod_id` (`prod_id`)
+		) ENGINE=InnoDB DEFAULT CHARSET=utf8; 
+    ");
+
+$table_orphaned = $installer->getTable('wsu_mediacontroll/orphaned');
+$installer->run("
+    DROP TABLE IF EXISTS `{$table_orphaned}`;
+    CREATE TABLE `{$table_orphaned}` (
+			`orphaned_id` int(11) unsigned NOT NULL auto_increment,
+			`filename` varchar(255) NOT NULL default '',
+			PRIMARY KEY  (`orphaned_id`),
+			UNIQUE KEY `filename` (`filename`)
+		) ENGINE=InnoDB DEFAULT CHARSET=utf8; 
+    ");
+
+
+
 $installer->endSetup(); 
