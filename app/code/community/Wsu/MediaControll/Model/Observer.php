51d37ad62055f00c685cfcbf1ea3cc7154963e05
jeremyBass <jeremybass@cableone.net>
2014-05-05 15:51:57 -0700

    update the obsever

--- app/code/community/Wsu/MediaControll/Model/Observer.php
+++ app/code/community/Wsu/MediaControll/Model/Observer.php
@@ -1,9 +1,15 @@
 <?php
 class Wsu_Mediacontroll_Model_Observer {
 
-	public function checkProductChange(){
-		
-		
+	public function checkProductChange($observer) {
+        /**
+         * @var $product Mage_Catalog_Model_Product
+         * @var $user    Mage_Admin_Model_User
+         */
+        $product = $observer->getEvent()->getProduct();
+		if ($product->hasDataChanges()) {
+
+        }
 	}
 
 
