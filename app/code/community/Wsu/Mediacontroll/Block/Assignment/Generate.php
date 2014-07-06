a2a8fb9b915129d843008b673841e1a4106a9c22
Jeremy Bass <jeremybass@cableone.net>
2014-07-06 00:25:29 -0700

    create an array of the product ids

--- app/code/community/Wsu/Mediacontroll/Block/Assignment/Generate.php
+++ app/code/community/Wsu/Mediacontroll/Block/Assignment/Generate.php
@@ -11,7 +11,11 @@ class Wsu_Mediacontroll_Block_Assignment_Generate extends Mage_Adminhtml_Block_T
 
     public function getProducts() {
 		$collection = Mage::getModel('catalog/product')->getCollection()->addAttributeToSelect('entity_id');
-        return (array)$collection;
+		$pros=array();
+		foreach($collection as $_product){
+			$pros[]=$_product->getId();
+		}
+        return $pros;
     }
 
 
