9abbfbe1a13e81ca1b5c5162c7b6246a2bb1cd95
Jeremy Bass <jeremybass@cableone.net>
2014-07-06 00:46:03 -0700

    only edit the ones with a enabled status

--- app/code/community/Wsu/Mediacontroll/Block/Assignment/Generate.php
+++ app/code/community/Wsu/Mediacontroll/Block/Assignment/Generate.php
@@ -11,6 +11,7 @@ class Wsu_Mediacontroll_Block_Assignment_Generate extends Mage_Adminhtml_Block_T
 
     public function getProducts() {
 		$collection = Mage::getModel('catalog/product')->getCollection()->addAttributeToSelect('entity_id');
+		$collection->addAttributeToFilter('status', array('eq' => Mage_Catalog_Model_Product_Status::STATUS_ENABLED));
 		$pros=array();
 		foreach($collection as $_product){
 			$pros[]=$_product->getId();
